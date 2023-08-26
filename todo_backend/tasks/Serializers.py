03b591dd55256937cf25c8e3475d6dc22f4557c0
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2023-08-26 20:07:14 +0300

    create task details api and added its related list details and url

--- todo_backend/tasks/Serializers.py
+++ todo_backend/tasks/Serializers.py
@@ -24,8 +24,7 @@ class ListsSerializer(serializers.ModelSerializer):
         data['list_details_url'] = data.pop('list_details_url')
         return data
 
-
-class TaskSerializer(serializers.ModelSerializer):
+class TasksForListSerializer(serializers.ModelSerializer):
     # track_listing = serializers.HyperlinkedIdentityField(view_name='task_details',lookup_field='id',)
 
     class Meta:
@@ -59,9 +58,46 @@ class ListTasksSerializer(serializers.ModelSerializer):
         exclude = []
 
 
-    tasks=TaskSerializer(many=True)
+    tasks=TasksForListSerializer(many=True)
     def to_representation(self, instance):
         data = super().to_representation(instance)
         data['tasks'] = data.pop('tasks')
+        return data
+
+
+class relatedListForTaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ListModel
+        fields = '__all__'
+        exclude = []
+
+    list_url = serializers.SerializerMethodField()
+
+    def get_list_url(self, obj):
+        request = self.context.get('request')
+        if request is not None:
+            track_listing_url = request.build_absolute_uri(
+                reverse('list_all_tasks', args=[obj.slug]))
+            return track_listing_url
+        return None
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['list_url'] = data.pop('list_url')
+        return data
+
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    related_list = relatedListForTaskSerializer()
+
+    class Meta:
+        model = TaskModel
+        fields = '__all__'
+        exclude = []
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['related_list'] = data.pop('related_list')
         return data
 
