ffea0ff88187c3f3683bfdb43fcfaf7c5459f56a
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2023-08-27 15:53:55 +0300

    frontend: showed tasks data

--- todo_frontend/src/Components/TaskItem.js
+++ todo_frontend/src/Components/TaskItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import deleteIcon from "../delete.png"
 
-const TaskItem = () => {
+const TaskItem = ({item}) => {
     return (
         <div style={{width:'calc(100% - 20px',backgroundColor:"white",borderRadius:"5px",height:"45px",flexShrink:"0"}} className={'d-flex justify-content-between px-2 align-items-center'}>
             <div>
-                <input type={'checkbox'} style={{    marginTop: "1px"}}/>
-                <label style={{color:"black"}} className={'px-2 py-0 my-0'}>text for a test</label>
+                <input type={'checkbox'} id={item.id} checked={item.isDone}/>
+                <label style={{color:"black",fontSize:"75%"}} className={'px-2 py-0 my-0'} htmlFor={item.id}>{item.title}</label>
             </div>
             <img style={{cursor:"pointer"}} width={'20px'} src={deleteIcon} />
         </div>
