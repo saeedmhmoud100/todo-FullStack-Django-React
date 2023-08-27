69b207585944344700a37cfcbc7971846abf86fe
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2023-08-27 13:59:23 +0300

    created add new task

--- todo_backend/tasks/models.py
+++ todo_backend/tasks/models.py
@@ -6,7 +6,7 @@ from django.utils.timezone import now
 
 class TaskModel(models.Model):
     title = models.CharField(max_length=30)
-    isDone = models.BooleanField(default=False)
+    isDone = models.BooleanField(default=False,blank=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=True)
     def __str__(self):
--- todo_backend/tasks/views.py
+++ todo_backend/tasks/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -7,11 +6,17 @@ from tasks.Serializers import TaskSerializer
 from tasks.models import TaskModel
 
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def all_tasks_view(request):
     if(request.method=='GET'):
         tasks = TaskSerializer(TaskModel.objects.all(),many=True,context={'request': request})
         return Response(tasks.data)
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def task_details_view(request,pk):
